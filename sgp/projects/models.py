94c8fbc49fef3344ae08786571a43500c5921f91
SpearsGoode <spearsgoode@protonmail.com>
2021-11-30 19:19:38 -0600

    add project model

--- sgp/projects/models.py
+++ sgp/projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=42)
+    date = models.DateField()
+    image = models.ImageField(upload_to='images/')
+    link = models.CharField(max_length=250)
+    info = models.CharField(max_length=1000)
+    etc = models.CharField(max_length=100000)
