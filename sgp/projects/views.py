9291f4a7daaaeb1417d45b10b2e0b92f1b98868f
SpearsGoode <spearsgoode@protonmail.com>
2021-11-30 23:40:38 -0600

    prep for bootstrap

--- sgp/projects/views.py
+++ sgp/projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def projects(request):
-    return render(request, 'projects/projects.html')
+    proj_list = Project.objects
+    return render(request, 'projects/projects.html', {'projects': proj_list})
